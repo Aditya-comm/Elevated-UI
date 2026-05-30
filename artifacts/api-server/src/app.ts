80dba549459bf42bd015d1808bd842f195c83b96
Aditya-comm <adityasethia08@gmail.com>
2026-05-30 10:13:08 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import router from "./routes";
+import router from "./routes/index.js";
 import { logger } from "./lib/logger";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
