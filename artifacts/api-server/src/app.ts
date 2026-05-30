dd1119cf22acda64619e120a4bb469722eb0e323
Aditya-comm <adityasethia08@gmail.com>
2026-05-30 10:15:21 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,14 @@
 import express, { type Express } from "express";
 import cors from "cors";
+import pinoHttp from "pino-http";
 import router from "./routes/index.js";
 import { logger } from "./lib/logger";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pinoHttp = require("pino-http");
 
 const app: Express = express();
 
 app.use(
   pinoHttp({
     logger,
-    serializers: {
-      req(req: any) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: any) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
   }),
 );
 
